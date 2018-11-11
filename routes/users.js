620af460dad7083782b3b7b31be48f7b03e0fb19
Kevin Lee <kevin.rph.lee@gmail.com>
2018-11-10 22:43:45 -0800

    switched double quotation to single

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     //Example Route
   });
 
