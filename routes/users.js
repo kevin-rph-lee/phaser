ed7d852b03e22b4545c8b8726ea821ca956adb35
Kevin Lee <kevin.rph.lee@gmail.com>
2018-11-10 22:54:15 -0800

    added users login and get route templates

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,32 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (knex) => {
+module.exports = (knex, bcrypt) => {
 
   router.get('/', (req, res) => {
-    //Example Route
+    knex
+      .select('*')
+      .from('users')
+      .then((results) => {
+        return res.sendStatus(300);
+      });
+  });
+
+  router.post('/login', (req, res) => {
+    knex
+      .select('*')
+      .from('users')
+      .where({email:req.body.email})
+      .then((results) => {
+        console.log(req.body);
+        if(req.body.password === undefined || results.length === 0 || req.body.password.length === 0){
+          return res.sendStatus(404);
+        } else if (bcrypt.compareSync(req.body.password, results[0].password)) {
+          return res.sendStatus(200);
+        } else {
+          return res.sendStatus(403);
+        }
+      });
   });
 
   return router;
